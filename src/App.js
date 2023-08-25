33b373f34196f576442555b3b5b23cf2e06688fb
AnanyaNunDesai <ananyadesai290402@gmail.com>
2023-08-25 14:33:01 +0530

    Added Functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import React from "react";
+import NavBar from "./components/NavBar";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
 
 export default function App() {
-  return "App";
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <NavBar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
 }
