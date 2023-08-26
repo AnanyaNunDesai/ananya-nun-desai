f8f64d537f260f8d980c11dd93af8ccefd21f298
AnanyaNunDesai <ananyadesai290402@gmail.com>
2023-08-27 00:29:11 +0530

    Added Music

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact";
-// import Music from "./components/BackgroundMusic";
+import Music from "./components/BackgroundMusic";
 
 export default function App() {
   return (
@@ -17,7 +17,7 @@ export default function App() {
       <Skills />
       <Testimonials />
       <Contact />
-      {/* <Music/> */}
+      <Music/>
     </main>
   );
 }
