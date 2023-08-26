1ed156be54ea95762e566f4851f16fc0465f44a8
AnanyaNunDesai <ananyadesai290402@gmail.com>
2023-08-26 15:22:09 +0530

    Added Hyperlinks and Socials

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -21,7 +21,9 @@ const Projects = () => {
                     {projects.map((project) => (
                         <a
                             href={project.link}
+                            target="_blank"
                             key={project.image}
+                            rel="noreferrer"
                             className="sm:w-1/2 w-100 p-4">
                             <div className="flex relative">
                                 <img
--- src/data.js
+++ src/data.js
@@ -4,32 +4,32 @@ export const projects = [
     subtitle: "React and Flask",
     description:
       "Creates a stego image of the highest possible quality for all your secret communication needs.",
-    image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
+    image: "./Stego-Gif.gif",
+    link: "https://github.com/AnanyaNunDesai/final-year-stego-proj",
   },
   {
-    title: "To-Do App",
+    title: "Note-Making App",
     subtitle: "Java",
     description:
-      "Cute little Android app to help you complete your tasks. Customizable colors for the best user experience.",
-    image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+      "Cute little Android app to help you take notes on-the-go. Customizable colors for the best user experience.",
+    image: "./Simple-Notes-App-Bordered.gif",
+    link: "https://github.com/AnanyaNunDesai/android-to-do-list",
   },
   {
     title: "Investment Tracker App",
     subtitle: "React Hooks",
     description:
-      "Always invest with caution, and keep track of all your investments.",
-    image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
+      "Always invest with caution, and keep track of all your investments. Calculate your gains and losses.",
+    image: "./Investments-Gif.gif",
+    link: "https://github.com/AnanyaNunDesai/investments-app",
   },
   {
     title: "Expense Tracker App",
     subtitle: "React",
     description:
-      "As a broke college student, this expense tracker is an absolute necessity.",
-    image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
+      "As a broke college student, this expense tracker is an absolute necessity. Sort by year with an engaging UI.",
+    image: "./Expenses-Gif.gif",
+    link: "https://github.com/AnanyaNunDesai/finance-app2",
   },
 ];
 
@@ -51,10 +51,10 @@ export const testimonials = [
 ];
 
 export const skills = [
-  "JavaScript",
   "ReactJS",
-  "Next.js",
-  "Node",
+  "JavaScript",
   "Figma",
   "Git",
+  "Adobe PhotoShop",
+  "Node",
 ];
